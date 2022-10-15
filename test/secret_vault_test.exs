95fce93fe83df2877509f864dc3b4556060bd405
yunmikun2 <yunmikun2@protonmail.com>
2022-10-15 21:34:54 +0300

    feat: Add storage

--- test/secret_vault_test.exs
+++ test/secret_vault_test.exs
@@ -1,8 +1,31 @@
 defmodule SecretVaultTest do
-  use ExUnit.Case
-  doctest SecretVault
+  use ExUnit.Case, async: true
 
-  test "greets the world" do
-    assert SecretVault.hello() == :world
+  alias SecretVault.{Config, FSResolver}
+
+  test "you can reed a secret after you've written it" do
+    config = Config.new(:secret_vault)
+    key = "test key"
+    environment = "#{Mix.env()}"
+    name = "secret_name_#{Enum.random(0..1000)}"
+    data = "test data"
+
+    on_exit(fn ->
+      path_for_file_with_secret =
+        FSResolver.resolve_file_path(
+          config,
+          environment,
+          name
+        )
+
+      File.rm!(path_for_file_with_secret)
+    end)
+
+    assert :ok = SecretVault.put_secret(config, key, environment, name, data)
+
+    assert {:ok, read} =
+             SecretVault.fetch_secret(config, key, environment, name)
+
+    assert read == data
   end
 end
